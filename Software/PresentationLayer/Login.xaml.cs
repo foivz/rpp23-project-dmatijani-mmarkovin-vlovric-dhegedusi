91324b9c0ae5d1b2f5dead7d3bbc1bc0acd41d07
Magdalena Markovinović <mmarkovin21@foi.hr>
2023-12-10 15:00:52 +0100

    Popravljen dizajn gumba i textboxa, dodane animacije za textHint-ove

--- Software/PresentationLayer/Login.xaml.cs
+++ Software/PresentationLayer/Login.xaml.cs
@@ -15,14 +15,42 @@ using System.Windows.Shapes;
 
 namespace PresentationLayer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void lblKorime_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            lblKorime.Focus();
+        }
+
+        private void txtKorime_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtKorime.Text) && txtKorime.Text.Length > 0) {
+                txtKorime.Visibility = Visibility.Collapsed;
+            } else
+            {
+                txtKorime.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void txtPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtPassword.Password) && txtPassword.Password.Length > 0)
+            {
+                txtPassword.Visibility = Visibility.Collapsed;
+            } else
+            {
+                txtPassword.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void lblPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtPassword.Focus();
         }
     }
 }
