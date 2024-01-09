c8333f46f8e5199171c8129c7e8c5b3287250f39
David Matijanić <dmatijani21@foi.hr>
2024-01-09 12:29:27 +0100

    Dovršen novi PresentationLayer

---
+++ Software/E_Libra/PresentationLayer/Login.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PresentationLayer {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void lblKorime_MouseDown(object sender, MouseButtonEventArgs e) {
+            txtKorime.Focus();
+        }
+
+        private void txtKorime_TextChanged(object sender, TextChangedEventArgs e) {
+            if (!string.IsNullOrEmpty(txtKorime.Text) && txtKorime.Text.Length > 0) {
+                lblKorime.Visibility = Visibility.Collapsed;
+            } else {
+                lblKorime.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void txtPassword_PasswordChanged(object sender, RoutedEventArgs e) {
+            if (!string.IsNullOrEmpty(txtPassword.Password) && txtPassword.Password.Length > 0) {
+                lblPassword.Visibility = Visibility.Collapsed;
+            } else {
+                lblPassword.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void lblPassword_MouseDown(object sender, MouseButtonEventArgs e) {
+            txtPassword.Focus();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e) {
+            MemberPanel userPanel = new MemberPanel();
+            Hide();
+            userPanel.ShowDialog();
+            Close();
+        }
+    }
+}
