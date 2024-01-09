34f7b97038aed436e55215f8cdd57c01d9206bc7
Magdalena Markovinović <mmarkovin21@foi.hr>
2024-01-09 20:17:48 +0100

    Stvoreni servisi za upravljanje logikom prijave i odjave korisnika.

--- Software/E_Libra/BussinessLogicLayer/services/MemberService.cs
+++ Software/E_Libra/BussinessLogicLayer/services/MemberService.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BussinessLogicLayer
+namespace BussinessLogicLayer.services
 {
-    public class Class1
+    public class MemberService
     {
     }
 }
