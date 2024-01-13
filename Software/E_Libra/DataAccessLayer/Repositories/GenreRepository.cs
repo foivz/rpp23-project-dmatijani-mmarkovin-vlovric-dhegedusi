9b92e00ae77073a99c39d7b23a0fc1d8f9d38493
Viktor Lovric <vlovric21@student.foi.hr>
2024-01-13 20:42:09 +0100

    Stvoren genre repository za sljedeći korak

---
+++ Software/E_Libra/DataAccessLayer/Repositories/GenreRepository.cs
@@ -0,0 +1,24 @@
+﻿using EntitiesLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenreRepository: Repository<Genre>
+    {
+        public GenreRepository(): base(new DatabaseModel())
+        {
+            
+        }
+
+        public override int Update(Genre entity, bool saveChanges = true)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
