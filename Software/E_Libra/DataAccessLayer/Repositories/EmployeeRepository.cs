2b0759e8b403a9053b6eebf352a00d092361d587
David Matijanić <dmatijani21@foi.hr>
2024-01-14 19:56:07 +0100

    Dodane metode u repozitorij za zaposlenike za dodavanje novog te ažuriranje postojećeg zaposlenika

--- Software/E_Libra/DataAccessLayer/Repositories/EmployeeRepository.cs
+++ Software/E_Libra/DataAccessLayer/Repositories/EmployeeRepository.cs
@@ -19,8 +19,41 @@ namespace DataAccessLayer.Repositories {
             return query;
         }
 
-        public override int Update(Employee entity, bool saveChanges = true) {
-            throw new NotImplementedException();
+        public override int Add(Employee employee, bool saveChanges = true) {
+            var library = Context.Libraries.SingleOrDefault(l => l.id == employee.Library.id);
+
+            var newEmployee = new Employee {
+                id = employee.id,
+                name = employee.name,
+                surname = employee.surname,
+                username = employee.username,
+                password = employee.password,
+                Library = library
+            };
+
+            Entities.Add(newEmployee);
+            if (saveChanges) {
+                return SaveChanges();
+            } else {
+                return 0;
+            }
+        }
+
+        public override int Update(Employee employee, bool saveChanges = true) {
+            var library = Context.Libraries.SingleOrDefault(l => l.id == employee.Library.id);
+
+            var existingEmployee = Entities.SingleOrDefault(e => e.id == employee.id);
+            existingEmployee.name = employee.name;
+            existingEmployee.surname = employee.surname;
+            existingEmployee.username = employee.username;
+            existingEmployee.password = employee.password;
+            existingEmployee.Library = library;
+
+            if (saveChanges) {
+                return SaveChanges();
+            } else {
+                return 0;
+            }
         }
     }
 }
