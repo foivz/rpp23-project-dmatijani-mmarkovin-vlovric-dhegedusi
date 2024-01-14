a35c1304093e77f99b3133cccf6b6c3406614d49
David Matijanić <dmatijani21@foi.hr>
2024-01-14 14:54:08 +0100

    Napravljen prikaz svih zaposlenika odabrane knjižnice

--- Software/E_Libra/PresentationLayer/AdminPanels/UcAllEmployees.xaml.cs
+++ Software/E_Libra/PresentationLayer/AdminPanels/UcAllEmployees.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BussinessLogicLayer.services;
+using EntitiesLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,17 +16,37 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PresentationLayer.AdminPanels {
-    /// <summary>
-    /// Interaction logic for UcAllEmployees.xaml
-    /// </summary>
     public partial class UcAllEmployees : UserControl {
+        private EmployeeService service = new EmployeeService();
+
         public UcAllEmployees() {
             InitializeComponent();
+            PopulateComboBox();
         }
 
         private void btnAddNewEmployee_Click(object sender, RoutedEventArgs e) {
             UcNewEmployee ucNewEmployee = new UcNewEmployee();
             AdminGuiControl.LoadNewControl(ucNewEmployee);
+        }
+
+        private void PopulateComboBox() {
+            var libraryService = new LibraryService();
+            cboLibrary.ItemsSource = libraryService.GetAllLibraries();
+        }
+
+        private void cboLibrary_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            Library selectedLibrary = cboLibrary.SelectedItem as Library;
+            LoadEmployees(selectedLibrary);
+        }
+        
+        private void LoadEmployees(Library selectedLibrary) {
+            if (selectedLibrary == null) {
+                dgAllEmployees.ItemsSource = new List<Library>();
+                return;
+            }
+
+            List<Employee> employees = service.GetEmployeesByLibrary(selectedLibrary);
+            dgAllEmployees.ItemsSource = employees;
         }
     }
 }
