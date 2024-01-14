fecb584dff88171c24623a626115cb0a71a66e1e
David Matijanić <dmatijani21@foi.hr>
2024-01-14 02:07:00 +0100

    Napravljen LibraryRepository

---
+++ Software/E_Libra/DataAccessLayer/Repositories/LibraryRepository.cs
@@ -0,0 +1,18 @@
+﻿using EntitiesLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories {
+    public class LibraryRepository : Repository<Library> {
+        public LibraryRepository(DatabaseModel context) : base(context) {
+
+        }
+
+        public override int Update(Library entity, bool saveChanges = true) {
+            return 0; // TODO
+        }
+    }
+}
