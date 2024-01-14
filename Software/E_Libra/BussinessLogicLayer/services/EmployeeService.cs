4c64e95c2ba8978151fc5072504ebb4a41c49213
David Matijanić <dmatijani21@foi.hr>
2024-01-14 14:35:39 +0100

    Napravljeni EmployeeRepository i LibraryRepository

---
+++ Software/E_Libra/BussinessLogicLayer/services/EmployeeService.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLayer.Repositories;
+using EntitiesLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLogicLayer.services {
+    public class EmployeeService {
+        public List<Employee> GetEmployeesByLibrary(Library library) {
+            using (var repository = new EmployeeRepository()) {
+                return repository.GetEmployeesByLibrary(library).ToList();
+            }
+        }
+    }
+}
---
+++ Software/E_Libra/DataAccessLayer/Repositories/EmployeeRepository.cs
@@ -0,0 +1,26 @@
+﻿using EntitiesLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories {
+    public class EmployeeRepository : Repository<Employee> {
+        public EmployeeRepository() : base(new DatabaseModel()) {
+
+        }
+
+        public IQueryable<Employee> GetEmployeesByLibrary(Library library) {
+            var query = from e in Entities
+                        where e.Library_id == library.id
+                        select e;
+
+            return query;
+        }
+
+        public override int Update(Employee entity, bool saveChanges = true) {
+            throw new NotImplementedException();
+        }
+    }
+}
