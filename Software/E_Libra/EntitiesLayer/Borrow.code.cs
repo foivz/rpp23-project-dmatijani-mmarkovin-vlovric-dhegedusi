3d46992173c1c3890250efd467e5dd54d26fb2d4
David Matijanić <dmatijani21@foi.hr>
2024-01-17 17:15:43 +0100

    Napravljena nova enumeracija za status posudbe

---
+++ Software/E_Libra/EntitiesLayer/Borrow.code.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesLayer {
+    public enum BorrowStatus {
+        Waiting,
+        Borrowed,
+        Late,
+        Returned
+    }
+
+    public partial class Borrow {
+    }
+}
