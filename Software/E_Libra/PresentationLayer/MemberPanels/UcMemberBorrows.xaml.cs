43d2099f046bd1f9ae0e81b1f15f70e5597eaa42
David Matijanić <dmatijani21@foi.hr>
2024-01-19 16:27:16 +0100

    Napravljeno dohvaćanje svih posudbi za prijavljenog člana

--- Software/E_Libra/PresentationLayer/MemberPanels/UcMemberBorrows.xaml.cs
+++ Software/E_Libra/PresentationLayer/MemberPanels/UcMemberBorrows.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BussinessLogicLayer.services;
+using EntitiesLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,8 +20,28 @@ namespace PresentationLayer.MemberPanels {
     /// Interaction logic for UcMemberBorrows.xaml
     /// </summary>
     public partial class UcMemberBorrows : UserControl {
+        private BorrowService borrowService = new BorrowService();
+        private MemberService memberService = new MemberService();
+
         public UcMemberBorrows() {
             InitializeComponent();
+
+            int memberId = memberService.GetMemberId(LoggedUser.Username);
+            int libraryId = memberService.GetMemberLibraryId(LoggedUser.Username);
+
+            GetAllBorrowsForMember(memberId, libraryId);
+            GetBorrowsForEachStatus(memberId, libraryId);
+        }
+
+        private void GetAllBorrowsForMember(int memberId, int libraryId) {
+            dgAllBorrows.ItemsSource = borrowService.GetAllBorrowsForMember(memberId, libraryId);
+        }
+
+        private void GetBorrowsForEachStatus(int memberId, int libraryId) {
+            dgPendingBorrows.ItemsSource = borrowService.GetBorrowsForMemberByStatus(memberId, libraryId, BorrowStatus.Waiting);
+            dgCurrentBorrows.ItemsSource = borrowService.GetBorrowsForMemberByStatus(memberId, libraryId, BorrowStatus.Borrowed);
+            dgLateBorrows.ItemsSource = borrowService.GetBorrowsForMemberByStatus(memberId, libraryId, BorrowStatus.Late);
+            dgDoneBorrows.ItemsSource = borrowService.GetBorrowsForMemberByStatus(memberId, libraryId, BorrowStatus.Returned);
         }
     }
 }
