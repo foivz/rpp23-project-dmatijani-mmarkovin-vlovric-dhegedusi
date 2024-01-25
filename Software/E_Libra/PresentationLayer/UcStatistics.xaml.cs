8465ceca024546b59dc8dde69bc7270a584b61be
Domagoj Hegedušić <dhegedusi21@foi.hr>
2024-01-25 12:35:02 +0100

    Napravljen dizajn i početna logika ekrana za statistiku.

--- Software/E_Libra/PresentationLayer/UcStatistics.xaml.cs
+++ Software/E_Libra/PresentationLayer/UcStatistics.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,7 +22,48 @@ namespace PresentationLayer {
     public partial class UcStatistics : UserControl {
         public UcStatistics() {
             InitializeComponent();
-
+            cmbStats.SelectionChanged += StatsComboBoxControl_SelectionChanged;
+            dgMostPopularBooks.Visibility = System.Windows.Visibility.Collapsed;
         }
+
+        private void StatsComboBoxControl_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            if (cmbStats.SelectedItem != null) {
+                int selectedOption = cmbStats.SelectedIndex;
+
+
+                switch (selectedOption) {
+                    case 0: // Najposuđenije knjige
+
+                        dgMostPopularBooks.Visibility = System.Windows.Visibility.Visible;
+                        break;
+
+                    case 1: // Broj ukupnih posudbi po žanru
+
+                        MessageBox.Show("Showing total borrowings per genre");
+                        break;
+
+                    case 2: // Broj registriranih članova
+
+                        MessageBox.Show("Showing total registered members");
+                        break;
+
+
+                    case 3: // Broj napisanih recenzija
+
+                        MessageBox.Show("Showing total registered members");
+                        break;
+
+                    case 4: // Prihodi
+
+                        MessageBox.Show("Showing total registered members");
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+
     }
 }
