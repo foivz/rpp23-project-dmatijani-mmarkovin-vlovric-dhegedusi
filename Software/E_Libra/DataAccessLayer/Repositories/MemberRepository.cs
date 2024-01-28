8ecb8bbe2c3e049da2a57001a358d06a1c821016
David Matijanić <dmatijani21@foi.hr>
2024-01-28 15:52:32 +0100

    Dodane metode za dobivanje barkoda knjige i člana na temelju njihovog ID-a

--- Software/E_Libra/DataAccessLayer/Repositories/MemberRepository.cs
+++ Software/E_Libra/DataAccessLayer/Repositories/MemberRepository.cs
@@ -127,5 +127,13 @@ namespace DataAccessLayer.Repositories
 
             return query;
         }
+
+        public IQueryable<string> GetMemberBarcode(int id) {
+            var query = from m in Member
+                        where m.id == id
+                        select m.barcode_id;
+
+            return query;
+        }
     }
 }
