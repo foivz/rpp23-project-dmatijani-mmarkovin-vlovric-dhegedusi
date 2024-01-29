8b04f697e7bb6d6fd232732b4eab4c889adf71d3
vlovric21 <vlovric21@student.foi.hr>
2024-01-29 19:35:35 +0100

    Eksternalno otvaranje PDF-a sa Process.Start

--- Software/E_Libra/PresentationLayer/WinHelpLogin.xaml.cs
+++ Software/E_Libra/PresentationLayer/WinHelpLogin.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,6 +33,7 @@ namespace PresentationLayer {
 
         private void LoadPdfDocument() {
             var path = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "PDF", "User_documentation_login.pdf");
+            Process.Start(path);
             MessageBox.Show(path);
             pdfDocument = PdfDocument.Load(path);
             PdfViewer pdfViewer = new PdfViewer();
